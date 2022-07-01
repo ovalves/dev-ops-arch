3047f24d13b040a24de02a633500a81474ac87db
Vinicius Alves <cit.vinicius@unidas.com.br>
2022-07-01 14:25:57 -0300

    assorted changes

--- _curso/2_comunicacao_entre_sistemas/grpc/cmd/client/client.go
+++ _curso/2_comunicacao_entre_sistemas/grpc/cmd/client/client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 
 	"github.com/ovalves/sample-grpc/pb"
@@ -18,7 +19,9 @@ func main() {
 	defer connection.Close()
 
 	client := pb.NewUserServiceClient(connection)
-	AddUser(client)
+
+	// AddUser(client)
+	AddUserVerbose(client)
 }
 
 func AddUser(cliente pb.UserServiceClient) {
@@ -34,4 +37,30 @@ func AddUser(cliente pb.UserServiceClient) {
 	}
 
 	fmt.Println(res)
+}
+
+func AddUserVerbose(cliente pb.UserServiceClient) {
+	req := &pb.User{
+		Id:    "0",
+		Name:  "João",
+		Email: "j@j.com",
+	}
+
+	responseStream, err := cliente.AddUserVerbose(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Could not make gRPC request: %v", err)
+	}
+
+	for {
+		stream, err := responseStream.Recv()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatalf("Could not receive the msg: %v", err)
+		}
+
+		fmt.Println("Status:", stream.Status, " - ", stream.GetUser())
+	}
 }
