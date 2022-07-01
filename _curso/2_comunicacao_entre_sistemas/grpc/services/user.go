3cc0221a0c00ee82fa7eb91509e567f5a459ca4f
Vinicius Alves <vinicius_o.a@live.com>
2022-07-01 15:17:58 -0300

    [Insert your commit message here. Be sure to make it descriptive.]

--- _curso/2_comunicacao_entre_sistemas/grpc/services/user.go
+++ _curso/2_comunicacao_entre_sistemas/grpc/services/user.go
@@ -15,6 +15,7 @@ import (
 // 	mustEmbedUnimplementedUserServiceServer()
 // 	AddUserVerbose(ctx context.Context, in *User, opts ...grpc.CallOption) (UserService_AddUserVerboseClient, error)
 //  AddUsers(ctx context.Context, opts ...grpc.CallOption) (UserService_AddUsersClient, error)
+//  AddUserStreamBoth(ctx context.Context, opts ...grpc.CallOption) (UserService_AddUserStreamBothClient, error)
 // }
 
 type UserService struct {
@@ -94,6 +95,29 @@ func (*UserService) AddUsers(stream pb.UserService_AddUsersServer) error {
 		})
 
 		fmt.Println("Adding", req.GetName())
+	}
+}
+
+func (*UserService) AddUserStreamBoth(stream pb.UserService_AddUserStreamBothServer) error {
+
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+
+		if err != nil {
+			log.Fatalf("Error receiving stream from the client: %v", err)
+		}
+
+		err = stream.Send(&pb.UserResultStream{
+			Status: "Added",
+			User:   req,
+		})
+
+		if err != nil {
+			log.Fatalf("Error sending stream to the client: %v", err)
+		}
 	}
 }
 
