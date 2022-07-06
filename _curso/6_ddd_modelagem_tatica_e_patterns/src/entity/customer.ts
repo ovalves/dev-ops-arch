689a3cbb84bb13c281a133b77f2ebba7c9f7f2c3
Vinicius Alves <vinicius_o.a@live.com>
2022-07-06 17:20:04 -0300

    I just evaluated random code in my console

--- _curso/6_ddd_modelagem_tatica_e_patterns/src/entity/customer.ts
+++ _curso/6_ddd_modelagem_tatica_e_patterns/src/entity/customer.ts
@@ -1,30 +1,58 @@
 import Address from './value_objects/address';
 
 export default class Customer {
-    _id: string;
-    _name: string;
-    _address!: Address;
+    private _id: string;
+    private _name: string;
+    private _address!: Address;
+    private _active: boolean = true;
 
     constructor(id: string, name: string) {
         this._id = id;
         this._name = name;
+        this.validate();
     }
 
-    changeName(name: string) {
+    changeName(name: string): void {
         this._name = name;
         this.validate();
     }
 
-    changeAddress(address: Address) {
+    changeAddress(address: Address): void {
         this._address = address;
         this.validate();
     }
 
-    validate(): void {
-        // Valida a entidade
+    activate(): void {
+        if (this._address === undefined) {
+            throw new Error("Address is mandatory to activate a customer");
+        }
+
+        this._active = true;
     }
 
-    set Address(address: Address) {
+    deactivate(): void {
+        this._active = false;
+    }
+
+    isActive(): boolean {
+        return this._active;
+    }
+
+    validate(): void {
+        if (this._id.length === 0) {
+            throw new Error("Id is required");
+        }
+
+        if (this._name.length === 0) {
+            throw new Error("Name is required");
+        }
+    }
+
+    get name(): string {
+        return this._name;
+    }
+
+    set Address(address: Address): void {
         this._address = address;
     }
 }
