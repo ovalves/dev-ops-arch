d8c5b15659a971ed07727f0ddcb99488e43041c7
Vinicius Alves <cit.vinicius@unidas.com.br>
2022-07-08 19:32:10 -0300

    I really should've committed this when I finished it...

--- _curso/6_ddd_modelagem_tatica_e_patterns/src/main.ts
+++ _curso/6_ddd_modelagem_tatica_e_patterns/src/main.ts
@@ -9,7 +9,7 @@ const address = new Address("Rua cinco", 131, "12345-678", "São Paulo");
 customer.Address = address;
 
 // Order Aggregate
-const item1 = new OrderItem("1", "Item 1", 10);
-const item2 = new OrderItem("2", "Item 2", 15);
-const item3 = new OrderItem("3", "Item 3", 25);
+const item1 = new OrderItem("1", "Item 1", 10, "product1", 1);
+const item2 = new OrderItem("2", "Item 2", 15, "product2", 1);
+const item3 = new OrderItem("3", "Item 3", 25, "product3", 1);
 const order = new Order("1", "123", [item1, item2, item3]);
