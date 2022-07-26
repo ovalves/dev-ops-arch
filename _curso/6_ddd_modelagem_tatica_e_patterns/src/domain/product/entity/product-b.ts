97c38959e05fb0ed1b92a0e6272ab524fed259e8
Vinicius Alves <vinicius_o.a@live.com>
2022-07-26 13:44:00 -0300

    Check next commit for message.

--- _curso/6_ddd_modelagem_tatica_e_patterns/src/domain/product/entity/product-b.ts
+++ _curso/6_ddd_modelagem_tatica_e_patterns/src/domain/product/entity/product-b.ts
@@ -1,9 +1,9 @@
-import ProductInterface from "./product.interface";
+import ProductInterface from './product.interface';
 
-export default class Product implements ProductInterface {
+export default class ProductB implements ProductInterface {
     private _id: string;
     private _name: string;
-    private _price: number = 0;
+    private _price: number;
 
     constructor(id: string, name: string, price: number) {
         this._id = id;
@@ -21,7 +21,7 @@ export default class Product implements ProductInterface {
     }
 
     get price(): number {
-        return this._price;
+        return this._price * 2;
     }
 
     changeName(name: string): void {
@@ -36,15 +36,15 @@ export default class Product implements ProductInterface {
 
     validate(): boolean {
         if (this._id.length === 0) {
-            throw new Error("Id is required");
+            throw new Error('Id is required');
         }
 
         if (this._name.length === 0) {
-            throw new Error("Name is required");
+            throw new Error('Name is required');
         }
 
-        if (this._price <= 0) {
-            throw new Error("Price must be greater than zero");
+        if (this._price < 0) {
+            throw new Error('Price must be greater than zero');
         }
 
         return true;
