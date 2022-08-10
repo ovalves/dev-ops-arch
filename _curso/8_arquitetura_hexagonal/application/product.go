c3261670b3a7dd8f7082508066d2cfa6d287ba02
Vinicius Alves <vinicius_o.a@live.com>
2022-08-10 01:49:42 -0300

    ...

---
+++ _curso/8_arquitetura_hexagonal/application/product.go
@@ -0,0 +1,90 @@
+package application
+
+import (
+	"errors"
+
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+)
+
+const (
+	DISABLED = "disabled"
+	ENABLED  = "enabled"
+)
+
+type ProductInterface interface {
+	IsValid() (bool, error)
+	Enable() error
+	Disable() error
+	GetID() string
+	GetName() string
+	GetStatus() string
+	GetPrice() float64
+}
+
+type Product struct {
+	ID     string  `valid:"uuidv4"`
+	Name   string  `valid:"required"`
+	Price  float64 `valid:"float,optional"`
+	Status string  `valid:"required"`
+}
+
+func NewProduct() *Product {
+	product := Product{
+		ID:     uuid.NewV4().String(),
+		Status: DISABLED,
+	}
+	return &product
+}
+
+func (product *Product) IsValid() (bool, error) {
+	if product.Status == "" {
+		product.Status = DISABLED
+	}
+
+	if product.Status != ENABLED && product.Status != DISABLED {
+		return false, errors.New("the status must be enabled or disabled")
+	}
+
+	if product.Price < 0 {
+		return false, errors.New("the price must be greater or equal zero")
+	}
+
+	_, err := govalidator.ValidateStruct(product)
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
+func (product *Product) Enable() error {
+	if product.Price > 0 {
+		product.Status = ENABLED
+		return nil
+	}
+	return errors.New("the price must be greater than zero to enable the product")
+}
+
+func (product *Product) Disable() error {
+	if product.Price == 0 {
+		product.Status = DISABLED
+		return nil
+	}
+	return errors.New("the price must be zero in order to have the product disabled")
+}
+
+func (product *Product) GetID() string {
+	return product.ID
+}
+
+func (product *Product) GetName() string {
+	return product.Name
+}
+
+func (product *Product) GetStatus() string {
+	return product.Status
+}
+
+func (product *Product) GetPrice() float64 {
+	return product.Price
+}
