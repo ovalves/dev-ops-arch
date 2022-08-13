2bd707e10fd0c1e23896965256b36ab2dffac26b
Vinicius Alves <vinicius_o.a@live.com>
2022-08-13 10:49:31 -0300

    ...

--- _curso/8_arquitetura_hexagonal/main.go
+++ _curso/8_arquitetura_hexagonal/main.go
@@ -1,17 +1,11 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"database/sql"
-
-	_ "github.com/mattn/go-sqlite3"
-	db2 "github.com/ovalves/go-hexagonal/adapters/db"
-	"github.com/ovalves/go-hexagonal/application"
-)
+import "github.com/ovalves/go-hexagonal/cmd"
 
 func main() {
-	db, _ := sql.Open("sqlite3", "db.sqlite")
-	dbAdapter := db2.NewProductDb(db)
-	productService := application.NewProductService(dbAdapter)
-	product, _ := productService.Create("Celular 2", 50)
-	productService.Enable(product)
+	cmd.Execute()
 }
