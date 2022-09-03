4f73cb7689e8972bc269c987eda21a6184b28507
Vinicius Alves <vinicius_o.a@live.com>
2022-09-03 01:43:06 -0300

    ...

--- _curso/6_ddd_modelagem_tatica_e_patterns/python/src/main.py
+++ _curso/6_ddd_modelagem_tatica_e_patterns/python/src/main.py
@@ -4,6 +4,22 @@ from domain.entity.aggregate.order_item import OrderItem
 from domain.entity.aggregate.order import Order
 from domain.checkout.factory.order_factory import OrderFactory
 
+# Customer Aggregate
+customer = Customer(
+    "123",
+    "Customer"
+)
+address = Address("Rua cinco", 131, "12345-678", "São Paulo")
+customer.set_address(address)
+
+# // Order Aggregate
+item1 = OrderItem("1", "Item 1", 10, "product1", 1)
+item2 = OrderItem("2", "Item 2", 15, "product2", 1)
+item3 = OrderItem("3", "Item 3", 25, "product3", 1)
+order = Order("1", "123", [item1, item2, item3])
+
+print(order.total)
+
 props = {
     'id': 'order 1',
     'customer_id': '123',
@@ -26,19 +42,3 @@ props = {
 }
 
 OrderFactory().create(props)
-
-# # Customer Aggregate
-# customer = Customer(
-#     "123",
-#     "Customer"
-# )
-# address = Address("Rua cinco", 131, "12345-678", "São Paulo")
-# customer.set_address(address)
-
-# # // Order Aggregate
-# item1 = OrderItem("1", "Item 1", 10, "product1", 1)
-# item2 = OrderItem("2", "Item 2", 15, "product2", 1)
-# item3 = OrderItem("3", "Item 3", 25, "product3", 1)
-# order = Order("1", "123", [item1, item2, item3])
-
-# print(order.total)
