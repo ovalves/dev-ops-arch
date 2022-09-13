45c454853eb0dd4e2d6936f6150c4fd9364e0a3e
Vinicius Alves <vinicius_o.a@live.com>
2022-09-12 23:50:55 -0300

    ...

---
+++ _curso/11_microsservice/python/payment/src/domain/dtos/process_transaction_dto.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel
+
+class ProcessTransactionDTO(BaseModel):
+    external_code: str
+    credit_card_number: str
+    credit_card_cvv: str
+    credit_card_exp_date: str
+    total: int
