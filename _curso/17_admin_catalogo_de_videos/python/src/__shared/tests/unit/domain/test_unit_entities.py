403460d7c0ad90e02f10fba1a31f131ccfe6d7bc
Vinicius Alves <vinicius_o.a@live.com>
2022-09-22 17:08:39 -0300

    ...

--- _curso/17_admin_catalogo_de_videos/python/src/__shared/tests/unit/domain/test_unit_entities.py
+++ _curso/17_admin_catalogo_de_videos/python/src/__shared/tests/unit/domain/test_unit_entities.py
@@ -21,39 +21,41 @@ class TestEntityUnit(unittest.TestCase):
         self.assertIsInstance(Entity(), ABC)
 
     def test_set_unique_entity_id_and_props(self):
-        entity = StubEntity(prop1='value1', prop2='value2')
-        self.assertEqual(entity.prop1, 'value1')
-        self.assertEqual(entity.prop2, 'value2')
+        entity = StubEntity(prop1="value1", prop2="value2")
+        self.assertEqual(entity.prop1, "value1")
+        self.assertEqual(entity.prop2, "value2")
         self.assertIsInstance(entity.unique_entity_id, UniqueEntityId)
         self.assertEqual(entity.unique_entity_id.id, entity.id)
 
     def test_accept_a_valid_uuid(self):
         entity = StubEntity(
-            unique_entity_id=UniqueEntityId(
-                '97eea803-f57b-4ae9-a0cf-b5a8c6ca98f5'
-            ),
-            prop1='value1',
-            prop2='value2',
+            unique_entity_id=UniqueEntityId("97eea803-f57b-4ae9-a0cf-b5a8c6ca98f5"),
+            prop1="value1",
+            prop2="value2",
         )
 
-        self.assertEqual(entity.id, '97eea803-f57b-4ae9-a0cf-b5a8c6ca98f5')
+        self.assertEqual(entity.id, "97eea803-f57b-4ae9-a0cf-b5a8c6ca98f5")
 
     def test_convert_entity_to_dict(self):
         entity = StubEntity(
-            unique_entity_id=UniqueEntityId(
-                '97eea803-f57b-4ae9-a0cf-b5a8c6ca98f5'
-            ),
-            prop1='value1',
-            prop2='value2',
+            unique_entity_id=UniqueEntityId("97eea803-f57b-4ae9-a0cf-b5a8c6ca98f5"),
+            prop1="value1",
+            prop2="value2",
         )
-        self.assertDictEqual(entity.to_dict(), {
-            'id': '97eea803-f57b-4ae9-a0cf-b5a8c6ca98f5',
-            'prop1': 'value1',
-            'prop2': 'value2'
-        })
+        self.assertDictEqual(
+            entity.to_dict(),
+            {
+                "id": "97eea803-f57b-4ae9-a0cf-b5a8c6ca98f5",
+                "prop1": "value1",
+                "prop2": "value2",
+            },
+        )
 
     def test_set_method(self):
-        entity = StubEntity(prop1='value1', prop2='value2',)
+        entity = StubEntity(
+            prop1="value1",
+            prop2="value2",
+        )
         # pylint: disable=protected-access
-        entity._set('prop1', 'changed')
-        self.assertEqual(entity.prop1, 'changed')
+        entity._set("prop1", "changed")
+        self.assertEqual(entity.prop1, "changed")
--- _curso/17_admin_catalogo_de_videos/python/src/category/tests/unit/domain/test_unit_entities.py
+++ _curso/17_admin_catalogo_de_videos/python/src/category/tests/unit/domain/test_unit_entities.py
@@ -6,53 +6,52 @@ from datetime import datetime
 from category.domain.entities import Category
 from dataclasses import is_dataclass, FrozenInstanceError
 
-class TestCategoryUnit(unittest.TestCase):
 
+class TestCategoryUnit(unittest.TestCase):
     def test_if_is_a_dataclass(self):
         self.assertTrue(is_dataclass(Category))
 
     def test_constructor(self):
         created_at = datetime.now()
         category = Category(
-            name='Movie',
-            description='some description',
+            name="Movie",
+            description="some description",
             is_active=False,
-            created_at=created_at
+            created_at=created_at,
         )
 
-        self.assertEqual(category.name, 'Movie')
-        self.assertEqual(category.description, 'some description')
+        self.assertEqual(category.name, "Movie")
+        self.assertEqual(category.description, "some description")
         self.assertEqual(category.is_active, False)
         self.assertEqual(category.created_at, created_at)
         self.assertIsInstance(category.created_at, datetime)
 
     def test_if_created_at_is_generated_in_constructor(self):
-        category1 = Category(name='Movie 1')
+        category1 = Category(name="Movie 1")
         time.sleep(0.1)
-        category2 = Category(name='Movie 2')
+        category2 = Category(name="Movie 2")
 
         self.assertNotEqual(
-            category1.created_at.timestamp(),
-            category2.created_at.timestamp()
+            category1.created_at.timestamp(), category2.created_at.timestamp()
         )
 
     def test_UniqueEntityId_is_immutable(self):
         with self.assertRaises(FrozenInstanceError):
-            value_object = Category(name='Movie 1')
-            value_object.name = 'Movie 3'
+            value_object = Category(name="Movie 1")
+            value_object.name = "Movie 3"
 
     def test_update(self):
-        category = Category(name='Movie')
-        category.update('Documentary', 'some description')
-        self.assertEqual(category.name, 'Documentary')
-        self.assertEqual(category.description, 'some description')
+        category = Category(name="Movie")
+        category.update("Documentary", "some description")
+        self.assertEqual(category.name, "Documentary")
+        self.assertEqual(category.description, "some description")
 
     def test_activate(self):
-        category = Category(name='Movie', is_active=False)
+        category = Category(name="Movie", is_active=False)
         category.activate()
         self.assertTrue(category.is_active)
 
     def test_deactivate(self):
-        category = Category(name='Movie')
+        category = Category(name="Movie")
         category.deactivate()
         self.assertFalse(category.is_active)
