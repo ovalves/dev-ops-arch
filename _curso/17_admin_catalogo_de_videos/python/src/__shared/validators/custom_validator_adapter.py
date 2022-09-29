7c520665c25c2cd115619ed83edf1b5afdab6767
Vinicius Alves <vinicius_o.a@live.com>
2022-09-28 21:10:56 -0300

    ...

--- _curso/17_admin_catalogo_de_videos/python/src/__shared/validators/custom_validator_adapter.py
+++ _curso/17_admin_catalogo_de_videos/python/src/__shared/validators/custom_validator_adapter.py
@@ -10,21 +10,13 @@ from src.__shared.validators.interface import ValidatorFieldsInterface
 class CustomValidatorAdapter(
     ValidatorFieldsInterface[PropsValidated], ABC
 ):  # pylint: disable=too-few-public-methods
-    """Validador customizado de campos de uma classe
-
-    Raises:
-        ValidationException: Quando o campo é obrigatório
-        ValidationException: Quando o campo precisa ser string
-        ValidationException: Quando ultrapassar o limite de caracteres
-        ValidationException: Quando o campo precisa ser boolean
-
+    """Adaptador de validação do Django Rest Framework
     Usage:
-        CustomValidatorAdapter.values(name, "name").required().string().max_length(255)
-        CustomValidatorAdapter.values(description, "description").string()
+        ---
 
     Returns:
-        CustomValidatorAdapter: Quando não houve erros na validação dos campos
-        ValidationException: Quando houve erros na validação dos campos
+        True: Quando os campos são válidos
+        False: Quando os campos NÃO são válidos
     """
 
     def validate(self, data) -> bool:
