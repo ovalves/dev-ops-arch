b6a4959145a135a308fb0c967edb1be846eb2b07
Vinicius Alves <vinicius_o.a@live.com>
2022-10-23 20:23:22 -0300

    ...

---
+++ 18_video_encoder/golang/domain/video_test.go
@@ -0,0 +1,16 @@
+package domain_test
+
+import (
+	"encoder/domain"
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+func TestValidateIfVideoIsEmpty(t *testing.T) {
+	video := domain.NewVideo()
+	err := video.Validate()
+
+	require.Error(t, err)
+}
