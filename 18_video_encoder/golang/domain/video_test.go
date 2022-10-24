9d7dd1bb7cca7259f43d034a2b31ddf6f67932ac
Vinicius Alves <vinicius_o.a@live.com>
2022-10-24 18:12:06 -0300

    ...

--- 18_video_encoder/golang/domain/video_test.go
+++ 18_video_encoder/golang/domain/video_test.go
@@ -14,3 +14,27 @@ func TestValidateIfVideoIsEmpty(t *testing.T) {
 
 	require.Error(t, err)
 }
+
+func TestVideoIdIsNotAUuid(t *testing.T) {
+	video := domain.NewVideo()
+
+	video.ID = "abc"
+	video.ResourceID = "a"
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	err := video.Validate()
+	require.Error(t, err)
+}
+
+func TestVideoValidation(t *testing.T) {
+	video := domain.NewVideo()
+
+	video.ID = uuid.NewV4().String()
+	video.ResourceID = "a"
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	err := video.Validate()
+	require.Nil(t, err)
+}
