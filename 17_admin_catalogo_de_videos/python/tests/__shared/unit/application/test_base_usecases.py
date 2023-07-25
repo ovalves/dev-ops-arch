d52a98e8781c0a942807a8cb50c07ecd1f966096
Vinicius Alves <vinicius_o.a@live.com>
2023-07-25 14:48:40 -0300

    ...

---
+++ 17_admin_catalogo_de_videos/python/tests/__shared/unit/application/test_base_usecases.py
@@ -0,0 +1,13 @@
+import unittest
+from src.__shared.application.usecases import UseCase
+
+
+class TestBaseUsecasesUnit(unittest.TestCase):
+    def test_throw_error_when_methods_not_implemented(self):
+        with self.assertRaises(TypeError) as assert_error:
+            UseCase()
+
+        self.assertEqual(
+            assert_error.exception.args[0],
+            "Can't instantiate abstract class UseCase with abstract method execute"
+        )
